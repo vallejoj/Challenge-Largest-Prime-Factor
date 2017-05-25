89907afca5c5762a4e7e67146bb1464baae06e53
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-05-24 16:40:10 -1000

    done

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,13 @@
 exports.largestPrimeFactor = function(n){
-  var primeNumber = 0;
-  // do your work here
 
+
+  var primeNumber=2;
+  while (primeNumber<=n){
+      if (n%primeNumber == 0){
+          n/=primeNumber;
+      }else{
+          primeNumber++;
+      }
+  }
   return primeNumber;
 };
